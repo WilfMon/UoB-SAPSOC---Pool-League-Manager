16bce46d33fb95a86c709d4265591d7623015795
Wilf <wilfmoncrieff1@gmail.com>
2026-09-18 10:24:12 +0100

    removed test.py

--- Client/test.py
+++
@@ -1,9 +0,0 @@
-from database.db import get_connection, get_player_matches_played, get_player_absence_streak ,recalculate_all_elo, complete_semester, remove_semester
-
-conn = get_connection()
-
-#print(get_player_matches_played(conn, 1))
-
-#print(get_player_absence_streak(conn, 70))
-
-remove_semester(conn, 5)
